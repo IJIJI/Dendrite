23c50457c154b9c5f02505bdc147dee1c25a63d0
IJssel <ijssel@synapt.nl>
2026-06-19 16:00:59 +0200

    rollback tokeniseer

--- src/language/parser/types.ts
+++ src/language/parser/types.ts
@@ -1,23 +1,6 @@
 import { SourceRef } from "../infra/nodes";
 import { RawProgram } from "../infra/program";
 
-//? Tokenization
-export type TokenKind =
-  | "keyword" // let, output
-  | "ident" // myVar, Filter, sourceBus
-  | "string" // "hello"
-  | "number" // 3, 3.14
-  | "boolean" // true, false
-  | "null" // null
-  | "punct" // ( ) [ ] { } , . : = => == != > >= < <= + - * / % !
-  | "eof";
-
-export interface Token {
-  kind: TokenKind;
-  value: string; // raw source text of this token
-  source: SourceRef;
-}
-
 
 //? Parsing
 export type ParseErrorKind =
