90f1b8c4afcc81e75aabcc4311b6da69582f0a7c
IJssel <ijssel@synapt.nl>
2026-06-21 13:14:28 +0200

    Added todo

--- src/language/analyser/analyser.ts
+++ src/language/analyser/analyser.ts
@@ -93,6 +93,7 @@ function checkCompat(actual: Type, expected: Type, name: string, ctx: AnalysisCo
       message: `Input '${name}' type '${typeToString(actual)}' is not compatible with expected '${typeToString(expected)}'`,
     });
   } else if (isAnyOrNull(actual) && !isAny(expected)) {
+    // TODO: Should null really be castable like any?
     ctx.warnings.push({
       kind: "implicit_any_cast",
       name,
